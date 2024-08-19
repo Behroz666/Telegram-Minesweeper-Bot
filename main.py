08b994fefebc55a3fa860f4c4e7efe5caecc7314
Behroz666 <BehrozBesharatnia@gmail.com>
2024-08-19 18:38:30 +0330

    added won & lost state + added error for clicking in opened areas

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, filters
 
 #TODO add game over and won view
-#TODO add more cosmetic stuff and rewrite the tutorial 
+#TODO add more cosmetic stuff and rewrite the tutorial (send it seprately)
+#TODO add reaction/emoji for lose or win
 #TODO make sure that first oppend cell is not bomb
-#TODO add a check list for cells to make  loops lighter
+#TODO add a check list for cells to make  loops more efficient
+#TODO add json handling for multiple users
 
 description = "**Minesweeper: Overview and How to Play**\n\n**Objective:**  \nThe goal of Minesweeper is to clear a grid of hidden mines without detonating any. The numbers revealed on the grid indicate how many mines are adjacent to that square, helping you deduce where the mines are hidden.\n\n**How to Play:**\n1. **Start by Clicking a Square:**  \n   - The first click will reveal a number or an empty space. An empty space indicates no adjacent mines.\n\n2. **Understand the Numbers:**  \n   - Each number on a revealed square shows how many mines are adjacent to it (including diagonals). Use this information to figure out where the mines might be.\n\n3. **Clear the Grid:**\n   - If you click on a mine, the game is over.\n   - The game is won when all non-mine squares are revealed.\n\n**Tips:**\n- Start with corners or edges to get better information.\n- If you're unsure, guess, but be cautious!\n\nEnjoy the challenge and improve your strategy with practice!"
+all = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64]
 no_after = [8,16,24,32,40,48,56,64]
 no_before = [1,9,17,25,33,41,49,57]
 flags = []
@@ -30,6 +33,19 @@ def  make_keyboard(apart):
     ]
     return(keyboard)
 
+def  make_done_keyboard(apart):
+    keyboard = [
+    [InlineKeyboardButton(apart[0][0], callback_data='done'),InlineKeyboardButton(apart[0][1], callback_data='done'), InlineKeyboardButton(apart[0][2], callback_data='done'), InlineKeyboardButton(apart[0][3], callback_data='done'), InlineKeyboardButton(apart[0][4], callback_data='done'), InlineKeyboardButton(apart[0][5], callback_data='done'), InlineKeyboardButton(apart[0][6], callback_data='done'), InlineKeyboardButton(apart[0][7], callback_data='done')],
+    [InlineKeyboardButton(apart[1][0], callback_data='done'),InlineKeyboardButton(apart[1][1], callback_data='done'), InlineKeyboardButton(apart[1][2], callback_data='done'), InlineKeyboardButton(apart[1][3], callback_data='done'), InlineKeyboardButton(apart[1][4], callback_data='done'), InlineKeyboardButton(apart[1][5], callback_data='done'), InlineKeyboardButton(apart[1][6], callback_data='done'), InlineKeyboardButton(apart[1][7], callback_data='done')],
+    [InlineKeyboardButton(apart[2][0], callback_data='done'),InlineKeyboardButton(apart[2][1], callback_data='done'), InlineKeyboardButton(apart[2][2], callback_data='done'), InlineKeyboardButton(apart[2][3], callback_data='done'), InlineKeyboardButton(apart[2][4], callback_data='done'), InlineKeyboardButton(apart[2][5], callback_data='done'), InlineKeyboardButton(apart[2][6], callback_data='done'), InlineKeyboardButton(apart[2][7], callback_data='done')],
+    [InlineKeyboardButton(apart[3][0], callback_data='done'),InlineKeyboardButton(apart[3][1], callback_data='done'), InlineKeyboardButton(apart[3][2], callback_data='done'), InlineKeyboardButton(apart[3][3], callback_data='done'), InlineKeyboardButton(apart[3][4], callback_data='done'), InlineKeyboardButton(apart[3][5], callback_data='done'), InlineKeyboardButton(apart[3][6], callback_data='done'), InlineKeyboardButton(apart[3][7], callback_data='done')],
+    [InlineKeyboardButton(apart[4][0], callback_data='done'),InlineKeyboardButton(apart[4][1], callback_data='done'), InlineKeyboardButton(apart[4][2], callback_data='done'), InlineKeyboardButton(apart[4][3], callback_data='done'), InlineKeyboardButton(apart[4][4], callback_data='done'), InlineKeyboardButton(apart[4][5], callback_data='done'), InlineKeyboardButton(apart[4][6], callback_data='done'), InlineKeyboardButton(apart[4][7], callback_data='done')],
+    [InlineKeyboardButton(apart[5][0], callback_data='done'),InlineKeyboardButton(apart[5][1], callback_data='done'), InlineKeyboardButton(apart[5][2], callback_data='done'), InlineKeyboardButton(apart[5][3], callback_data='done'), InlineKeyboardButton(apart[5][4], callback_data='done'), InlineKeyboardButton(apart[5][5], callback_data='done'), InlineKeyboardButton(apart[5][6], callback_data='done'), InlineKeyboardButton(apart[5][7], callback_data='done')],
+    [InlineKeyboardButton(apart[6][0], callback_data='done'),InlineKeyboardButton(apart[6][1], callback_data='done'), InlineKeyboardButton(apart[6][2], callback_data='done'), InlineKeyboardButton(apart[6][3], callback_data='done'), InlineKeyboardButton(apart[6][4], callback_data='done'), InlineKeyboardButton(apart[6][5], callback_data='done'), InlineKeyboardButton(apart[6][6], callback_data='done'), InlineKeyboardButton(apart[6][7], callback_data='done')],
+    [InlineKeyboardButton(apart[7][0], callback_data='done'),InlineKeyboardButton(apart[7][1], callback_data='done'), InlineKeyboardButton(apart[7][2], callback_data='done'), InlineKeyboardButton(apart[7][3], callback_data='done'), InlineKeyboardButton(apart[7][4], callback_data='done'), InlineKeyboardButton(apart[7][5], callback_data='done'), InlineKeyboardButton(apart[7][6], callback_data='done'), InlineKeyboardButton(apart[7][7], callback_data='done')],
+    ]
+    return(keyboard)
+
 def chunk(table):
     rows = []
     i = 1
@@ -42,7 +58,7 @@ def chunk(table):
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     global bomb_blocks, table, game, opened
     game = ["➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖","➖"]
-    bomb_blocks = random.sample(range(1, 65), 10)
+    bomb_blocks = sorted(random.sample(range(1, 65), 10))
     table = []
     opened=[]
     i = 1
@@ -102,7 +118,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(description, parse_mode='Markdown', reply_markup=InlineKeyboardMarkup(make_keyboard(chunk(game))))
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global flag, bomb, bomb_blocks, table, no_after, no_before, game
+    global flag, bomb, bomb_blocks, table, no_after, no_before, game, all
     query = update.callback_query
     #await query.answer()
     choice = query.data
@@ -114,6 +130,8 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         flag = False
         bomb = True
         await query.answer("Bomb mode enabled")
+    elif choice == "done":
+        await query.answer("Your game is done. use /start")
     else: 
         choice = int(choice)
         if flag:
@@ -130,9 +148,14 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         elif bomb:
             if choice in flags:
                 await query.answer("It's Flag! you have to remove it fisrt")
+            elif choice in opened:
+                await query.answer("Cell already openned")
             else: 
                 if choice in bomb_blocks:
-                    await query.edit_message_text("You lost",reply_markup=InlineKeyboardMarkup(make_keyboard(chunk(game)))) #TODO make function for a lost game with different call backs + make win state
+                    for i in bomb_blocks:
+                        game[i-1] = "💣"
+                    game[choice-1] = "🧨"
+                    await query.edit_message_text("You lost",reply_markup=InlineKeyboardMarkup(make_done_keyboard(chunk(game))))
                 else: 
                     pre_opened = copy.deepcopy(opened)
                     opened.append(choice)
@@ -209,8 +232,13 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                         continue
                     for i in opened:
                         game[i-1] = table[i-1]
-                    await query.edit_message_text(description, parse_mode='Markdown',reply_markup=InlineKeyboardMarkup(make_keyboard(chunk(game))))
-                    await query.answer("New cell opened")
+                    if sorted(opened) == list(set(all) - set(bomb_blocks)):
+                        for i in bomb_blocks:
+                            game[i-1] = "💣"
+                        await query.edit_message_text("You won",reply_markup=InlineKeyboardMarkup(make_done_keyboard(chunk(game))))
+                    else:
+                        await query.edit_message_text(description, parse_mode='Markdown',reply_markup=InlineKeyboardMarkup(make_keyboard(chunk(game))))
+                        await query.answer("New cell opened")
 
 def main() -> None:
     application = Application.builder().token('7538249939:AAEeQzgiD-42si5VkG0DQipTm7IwYo9unpk').build()
